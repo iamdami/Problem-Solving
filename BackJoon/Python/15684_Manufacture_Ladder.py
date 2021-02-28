0e4ebaede6db6e1fbb4692f031735a46487f1db5
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-28 17:12:55 +0900

    Create 15684_Manufacture_Ladder.py

---
+++ BackJoon/Python/15684_Manufacture_Ladder.py
@@ -0,0 +1,17 @@
+# pseudo code
+
+def check(i, j, k):
+  pass
+
+def result():
+  pass
+
+answer = 4
+
+for i in range(ALL):
+  for j in range(i, ALL):
+    for k in range(j, ALL):
+      p = check(i, j, k)
+      if p and result(): answer = min(answer, p)
+        
+print(answer)
